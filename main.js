87dc8da2e408770de8bf399591e4555190487e4f
mnmeyers <mmeyers218@gmail.com>
2015-07-19 14:57:25 -0700

    initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var ejs = require('ejs');
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public'));
+//routes:
+
+app.get('/', function(req,res){
+   res.render('index', {
+       title: 'Home'
+   });
+});
+
+app.listen((8000), function(){
+    console.log('listening on localhost:8000');
+});
+module.exports = app;
